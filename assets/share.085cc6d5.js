c4fbb0e267f23b578eddf10b6a58906aba85b2a3
jxjxxxxx <2605987661@qq.com>
2022-09-06 17:49:53 +0800

    1

--- assets/share.085cc6d5.js
+++
@@ -1,1 +0,0 @@
-import{i as a}from"./http.7cff67d6.js";const e=async s=>(await a.get("/api/oss/random-share-background",{})).data,i=async s=>(await a.get("/api/task/share/card-img-size",{})).data;export{e as b,i as s};
