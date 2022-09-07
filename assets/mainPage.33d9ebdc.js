6c6f3547a1552c97b54cab0d3e0034208e9ba39c
jxjxxxxx <2605987661@qq.com>
2022-09-07 17:10:00 +0800

    1

---
+++ assets/mainPage.33d9ebdc.js
@@ -0,0 +1,1 @@
+import{i as s}from"./http.171e5d90.js";const n=async a=>(await s.get("/api/user/credit",{headers:{token:a}})).data,r=async a=>(await s.get("/api/draw/chance",{})).data,c=async a=>(await s.post("/api/draw/buy-chance",{})).data,i=async a=>(await s.get("/api/task/",{})).data,o=async a=>(await s.post("/api/task/login",{})).data;export{n as a,c as b,r as c,o as l,i as t};
