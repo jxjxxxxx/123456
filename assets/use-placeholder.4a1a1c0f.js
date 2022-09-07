82435ee31902a5c86e79e996e187595427189012
jxjxxxxx <2605987661@qq.com>
2022-09-07 19:32:21 +0800

    1

--- assets/use-placeholder.4a1a1c0f.js
+++
@@ -1,1 +0,0 @@
-import{r,X as u,R as i,a2 as n,$ as h,e as c}from"./index.6044140a.js";const l=(o,s)=>{const e=r(),t=()=>{e.value=h(o).height};return u(()=>{if(i(t),s)for(let a=1;a<=3;a++)setTimeout(t,100*a)}),n(()=>i(t)),e};function d(o,s){const e=l(o,!0);return t=>c("div",{class:s("placeholder"),style:{height:e.value?`${e.value}px`:void 0}},[t()])}export{d as u};
