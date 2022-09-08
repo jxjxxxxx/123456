378eee573c16cf0a25198c41415a823bbfd01536
jxjxxxxx <2605987661@qq.com>
2022-09-08 17:55:55 +0800

    1

---
+++ assets/mainPage.5671a6f3.js
@@ -0,0 +1,1 @@
+import{i as s}from"./index.10f9eb63.js";const n=async a=>(await s.get("/api/user/credit",{headers:{token:a}})).data,r=async a=>(await s.get("/api/draw/chance",{})).data,c=async a=>(await s.post("/api/draw/buy-chance",{})).data,i=async a=>(await s.get("/api/task/",{})).data,o=async a=>(await s.post("/api/task/login",{})).data;export{n as a,c as b,r as c,o as l,i as t};
