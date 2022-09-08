e46b0153c537a5cb15637ded9e808fc1e635dd2c
jxjxxxxx <2605987661@qq.com>
2022-09-08 18:33:30 +0800

    1

---
+++ assets/draw.1c9d1506.js
@@ -0,0 +1,1 @@
+import{i as s}from"./index.3cdd6e5f.js";const n=async a=>(await s.post("/api/draw/draw-one",{})).data,o=async(a,r)=>(await s.get("/api/draw/records",{params:{current:a,size:r}})).data;export{n as d,o as g};
